45936a39fe9f8ab5185121088cd5b274ff414db0
LintangRaka <152347689+LintangRaka@users.noreply.github.com>
2024-03-31 14:02:47 +0800

    Add files via upload
    Mohon maaf sebelumnya pak, saya mengumpulkan secara manual dikarenakan pada saat saya push melalui cmd itu selalu eror padahal saya sudah mengikuti tutorial yang bapak berikan dari awal sampai akhir. Tapi selalu pada saat git push origin master selalu eror pak. Sekian terima kasih.

---
+++ Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Dosen ============================");
+  System.out.println("NIDN : 2210010381");
+  System.out.println("Nama : Lintang Raka Buana");
+  System.out.println("==========================================");
+ }
+}
